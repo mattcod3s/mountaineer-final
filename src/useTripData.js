da774a3441e4561c8d7fe0658490b7932bad0884
MattCod3s <matthew.kostka08@gmail.com>
2021-01-03 02:46:35 +0000

    pie chart data functionality complete, need to style pie chart

--- src/useTripData.js
+++ src/useTripData.js
@@ -1,19 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { PlannedTripsContext } from './context/context';
 
 const useTripData = () => {
-
     const { ConqueredTrips } = useContext(PlannedTripsContext);
+    const contCounter = [0,0,0,0,0,0];
     let total = ConqueredTrips.length;
-
-    const contCounter = [5,8,9,2,0,0];
+    const sumfunc = () => {
+        for(let i = 0; i<=6; i++) {
+            let arr = ConqueredTrips.filter((el) => el.continent === continents[i]);
+            contCounter[i] = arr.length;
+        }
+        return contCounter;
+    }
 
     const continents = ['ASIA', 'SOUTH_AMERICA', 'NORTH_AMERICA', 'AUSTRALIA', 'AFRICA', 'EUROPE'];
 
     const colors = ['red', 'blue', 'green', 'purple', 'yellow', 'orange'];
     const chartData = {
         datasets : [{
-            data : contCounter.map((t) => t),
+            data : sumfunc(),
             backgroundColor : colors.map((c) => c)
         }],
         labels : continents.map((c) => c)
